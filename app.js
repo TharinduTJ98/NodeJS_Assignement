d0090cf76fc4d20843cd103363ace120b6c94b27
Tharindu Jayawardhana <86124203+TharinduTJ98@users.noreply.github.com>
2023-11-30 09:26:02 +0530

    separate database connection

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const db = require('./connection')
 
 const app = express();
 const port = 3000;
-
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'assignmentdb',
-});
 
 db.connect(err => {
   if (err) {
